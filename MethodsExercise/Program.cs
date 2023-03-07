38ba6e6f2710809a322480c6e3fc547c1bda19a4
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-07 13:38:45 -0700

    exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1 method UserName
+
+            Console.WriteLine("What is your first name?");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var favColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            var favFood = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var favAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite hobby?");
+            var favHobby = Console.ReadLine();
+
+            UserName(firstName, lastName, favColor, favFood, favAnimal, favHobby);
+
+
         }
-    }
+
+        //Exercise 1 method UserName
+
+        public static void UserName(string nameFirst, string nameLast, string favColor, string favFood, string favAnimal, string favHobby)
+            
+        {
+            Console.WriteLine($"My name is {nameFirst} {nameLast}. Some of my favorite things are..." +
+                $"the color {favColor}, {favFood}, {favAnimal}, and {favHobby}. Nice to meet you!");
+          
+        }
+
+        
+
+    }   
 }
